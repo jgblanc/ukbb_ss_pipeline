d1389c4d47717eed6387e19587b9a07ff5543eff
Alex Stern <alexstern@Alexs-MacBook-Air.local>
2022-03-31 17:10:31 -0500

    Added error messages

--- code/get_iaf.R
+++ code/get_iaf.R
@@ -4,7 +4,7 @@
 
 args=commandArgs(TRUE)
 
-if(length(args) != 3) {stop("Please supply 3 inputs")}
+if(length(args) != 6) {stop("Please supply 6 inputs")}
 
 library(data.table)
 library(tidyverse)
@@ -12,77 +12,110 @@ library(boot)
 
 ss_file=args[1]
 blocks_file=args[2]
-output_file=args[3]
+code_file=args[3]
+maf_t=as.numeric(args[4])
+pval_t=as.numeric(args[5])
+output_file=args[6]
+print(maf_t)
+print(pval_t)
 
 # Read in summary statistics data
 df <- fread(ss_file)
+
+# Return the name of the phenotype
+file_split <- str_split(ss_file, "\\.")
+name_split <- str_split(file_split[[1]][1], "/")
+phenotype_code <- name_split[[1]][length(name_split[[1]])]
+
+# Read in the code to phenotype conversion file
+all_phenotype_codes <- fread(code_file, header=F)
+colnames(all_phenotype_codes) <- c("code", "phenotype")
+
+# Create table of phenotype names that match the code
+pheno_name_table <- all_phenotype_codes %>% filter(code == phenotype_code)
+phenotype_name <- as.character(pheno_name_table[1,"phenotype"])
+
 
 # Remove low confidence variants
 df_filtered <- df %>% filter(low_confidence_variant == FALSE)
 
 # Remove non-significant variants
-df_sig <- df_filtered %>% filter(pval < 5e-8)
+df_sig <- df_filtered %>% filter(pval < pval_t)
+if(nrow(df_sig) == 0) {
+  df_out <- as.data.frame(matrix(c(NA, NA, NA, NA, NA), nrow = 1))
+  colnames(df_out) <- c("mean_iaf", "lower_ci", "upper_ci", "SNP_number", "phenotype_name")
+  print("No significant SNPs")
+} else { 
 
-# Separated column 1 into chromosome, base pair, reference allele, and alt. allele
-df_split <- df_sig %>% separate(variant, c("chr", "bp", "ref", "alt"), sep = ":")
+  # Separated column 1 into chromosome, base pair, reference allele, and alt. allele
+  df_split <- df_sig %>% separate(variant, c("chr", "bp", "ref", "alt"), sep = ":")
 
-# Filtered out low minor allele frequency variants
-df_af_filter <- df_split %>% filter(minor_AF > 0.01)
+  # Filtered out low minor allele frequency variants
+  df_af_filter <- df_split %>% filter(minor_AF > maf_t)
 
-# Read in ld block information
-ld_block <- fread(blocks_file)
+  # Read in ld block information
+  ld_block <- fread(blocks_file)
 
-assign_SNP_to_block <- function(CHR, BP, block = ld_block) {
+  assign_SNP_to_block <- function(CHR, BP, block = ld_block) {
   
-  # Filter blocks based on snp
-  block_chr <- block %>% filter(chr == CHR)
-  first_start <- as.numeric(block_chr[1, "start"])
-  block_bp <- block_chr %>% filter( (start < BP & stop >= BP) | BP == first_start) 
+    # Filter blocks based on snp
+    block_chr <- block %>% filter(chr == CHR)
+    first_start <- as.numeric(block_chr[1, "start"])
+    block_bp <- block_chr %>% filter( (start < BP & stop >= BP) | BP == first_start) 
   
-  # Assign
-  block_num <- as.numeric(block_bp[,"block_number"])
-  return(block_num)
+    # Assign
+    block_num <- as.numeric(block_bp[,"block_number"])
+    return(block_num)
+  }
+
+
+  df_af_filter <- df_af_filter[1:2,] #Delete this line for final implementation
+  # Add block info - takes a while 
+  df_blocks <- df_af_filter %>% 
+    mutate(block = apply(., MARGIN = 1, FUN = function(params)assign_SNP_to_block(as.numeric(params[1]), as.numeric(params[2]))))
+
+  # Pick the minimum p-value per block
+  df_minP <- df_blocks %>%
+    group_by(block) %>%
+    slice_min(pval, with_ties = F)
+  
+  # Find the alternate allele frequency
+  df_alt_freq <- df_minP %>%
+    mutate(alt_AF = case_when(alt == minor_allele ~ minor_AF, alt != minor_allele ~ (1 - minor_AF)))
+  
+  # Find the increasing allele frequency
+  df_inc_freq <- df_alt_freq %>%
+    mutate(inc_AF = case_when((beta >= 0) ~ alt_AF, (beta < 0) ~ (1 - alt_AF)))
+  
+  # Record number of SNPs used to calculate average
+  nSNPs <- length(df_inc_freq$inc_AF)
+  
+  # Find average of the increasing allele frequency
+  avg_inc_AF <- mean(df_inc_freq$inc_AF)
+
+  if(nSNPs == 1) {
+    df_out <- as.data.frame(matrix(c(avg_inc_AF, avg_inc_AF, avg_inc_AF, 1, phenotype_name), nrow = 1))
+    colnames(df_out) <- c("mean_iaf", "lower_ci", "upper_ci", "SNP_number", "phenotype_name")
+    print("1 significant SNP")
+  } else {
+  
+    # Boot strap resampling 
+    boot_mean <- function(original_vector, resample_vector) {
+      mean(original_vector[resample_vector])
+    }
+  
+    # R is number of replications
+    mean_results <- boot(df_inc_freq$inc_AF, boot_mean, R = 5000)
+    ci <- boot.ci(boot.out = mean_results, type = "basic")
+  
+    # Create the confidence interval
+    lb <- ci$basic[4]
+    ub <- ci$basic[5]
+  
+    # Create output data for trait
+    df_out <- as.data.frame(matrix(c(avg_inc_AF, lb, ub, nSNPs, phenotype_name), nrow = 1))
+    colnames(df_out) <- c("mean_iaf", "lower_ci", "upper_ci", "SNP_number", "phenotype_name")
+  }
 }
-
-
-df_af_filter <- df_af_filter[1:1000,] #Delete this line for final implementation
-# Add block info - takes a while 
-df_blocks <- df_af_filter %>% 
-  mutate(block = apply(., MARGIN = 1, FUN = function(params)assign_SNP_to_block(as.numeric(params[1]), as.numeric(params[2]))))
-
-# Pick the minimum p-value per block
-df_minP <- df_blocks %>%
-  group_by(block) %>%
-  slice_min(pval, with_ties = F)
-
-# Find the alternate allele frequency
-df_alt_freq <- df_minP %>%
-  mutate(alt_AF = case_when(alt == minor_allele ~ minor_AF, alt != minor_allele ~ (1 - minor_AF)))
-
-# Find the increasing allele frequency
-df_inc_freq <- df_alt_freq %>%
-  mutate(inc_AF = case_when((beta >= 0) ~ alt_AF, (beta < 0) ~ (1 - alt_AF)))
-
-# Find average of the increasing allele frequency
-avg_inc_AF <- mean(df_inc_freq$inc_AF)
-
-# Boot strap resampling 
-boot_mean <- function(original_vector, resample_vector) {
-  mean(original_vector[resample_vector])
-}
-
-# R is number of replications
-mean_results <- boot(df_inc_freq$inc_AF, boot_mean, R = 5000)
-ci <- boot.ci(boot.out = mean_results, type = "basic")
-
-# Create the confidence interval
-lb <- ci$basic[4]
-ub <- ci$basic[5]
-
-# Create output data for trait
-df_out <- as.data.frame(matrix(c(avg_inc_AF, lb, ub), nrow = 1))
-colnames(df_out) <- c("mean_iaf", "lower_ci", "upper_ci")
-
-
 # Save output
 fwrite(df_out, output_file, row.names = F, col.names = T, sep = "\t")
--- snakefile
+++ snakefile
@@ -1,10 +1,15 @@
 # Pipeline to process GWAS summary statistics
 IDS, = glob_wildcards("data/raw/{id}.gwas.imputed_v3.both_sexes.tsv.bgz")
+MAF = ["maf_0.01"]
+PVAL = ["pval_10e-2000"]
 
+def get_params(x):
+	out = x.split("_")[1]
+	return out
 
 rule all:
 	input:
-		expand("data/iaf/{id}.gwas.imputed_v3.both_sexes.tsv", id=IDS)
+		expand("data/iaf/{maf}/{pval}/{id}.gwas.imputed_v3.both_sexes.tsv", id=IDS, maf=MAF, pval=PVAL)
 
 
 #This rule unzips raw bgz files
@@ -20,11 +25,15 @@ rule unzip_bgz:
 rule iaf:
 	input:
 		ss = "data/unzipped/{id}.gwas.imputed_v3.both_sexes.tsv",
-		blocks = "data/fourier_ls-all_parsed.bed"
+		blocks = "data/fourier_ls-all_parsed.bed",
+		codes = "data/ukbb_phenotype_names.tsv"
+	params:
+		maf_t = lambda wildcards: get_params(wildcards.maf),
+		pval_t = lambda wildcards: get_params(wildcards.pval)
 	output:
-		"data/iaf/{id}.gwas.imputed_v3.both_sexes.tsv"
+		"data/iaf/{maf}/{pval}/{id}.gwas.imputed_v3.both_sexes.tsv"
 	shell:
-		"Rscript code/get_iaf.R {input.ss} {input.blocks} {output}"
+		"Rscript code/get_iaf.R {input.ss} {input.blocks} {input.codes} {params.maf_t} {params.pval_t} {output}"
 
 
 
